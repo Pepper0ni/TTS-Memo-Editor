066fd4c537bf11c4ca34eee6bde6a7142eeaa7da
Pepper0ni <pepper0ni3steps@gmail.com>
2022-06-22 00:11:35 +0100

    clean up code

--- memoEditor.lua
+++ memoEditor.lua
@@ -2,42 +2,31 @@ function onLoad()
  local selfScale=self.getScale()
  local params={
  function_owner=self,
- label='Log data',
- tooltip="Logs object data",
  font_size=180,
  width=1500,
  height=220,
  scale={1/selfScale.x,1/selfScale.y,1/selfScale.z},
- position={0,0,1.2},
- click_function='logData'
  }
- self.createButton(params)
+ butWrapper(params,{0,0,1.2},'Log data',"Logs object data",'logData')
+ butWrapper(params,{0,0,1.5},'Get Object Memo',"Retrieves the object's Memo",'getMemo')
+ butWrapper(params,{0,0,2.1},'Set Memo',"Stores the current Memo on all objects on the tile",'setMemo')
+ butWrapper(params,{0,0,2.4},'Clear script_state',"Sets the object's script_state to nil","clearSave")
 
- params.position[3]=1.5
- params.label='Get Object Memo'
- params.tooltip="Retrieves the object's Memo"
- params.click_function='getMemo'
- self.createButton(params)
-
- params.position[3]=2.1
- params.label='Set Memo'
- params.tooltip="Stores the current Memo on all objects on the tile"
- params.click_function='setMemo'
- self.createButton(params)
-
- params.position[3]=2.4
- params.label='Clear script_state'
- params.tooltip="Sets the object's script_state to nil"
- params.click_function='clearSave'
- self.createButton(params)
-
- params.position[3]=1.8
+ params.position={0,0,1.8}
  params.tooltip="The object's Memo"
  params.label='Enter Memo'
  params.alignment=3
  params.input_function="dummyFunc"
  params.font_color={0,0,0}
  self.createInput(params)
+end
+
+function butWrapper(params,pos,label,tool,func)
+ params.position=pos
+ params.label=label
+ params.tooltip=tool
+ params.click_function=func
+ self.createButton(params)
 end
 
 function CheckForObjects()
